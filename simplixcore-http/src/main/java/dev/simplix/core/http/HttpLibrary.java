2f7bac6274cd9d7793d9a59a067291075207ded7
Nico Britze <n.britze@exceptionflug.de>
2020-08-19 18:11:14 +0200

    Optimize jar sizes. simplixcore-http is now a library

---
+++ simplixcore-http/src/main/java/dev/simplix/core/http/HttpLibrary.java
@@ -0,0 +1,6 @@
+package dev.simplix.core.http;
+
+import dev.simplix.core.common.aop.SimplixApplication;
+
+@SimplixApplication(name = "SimplixCoreHttp", version = "1.0", authors = "SimplixSoftworks")
+public class HttpLibrary {}
