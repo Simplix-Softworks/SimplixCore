dffca6246540a7f2b048c951c60a76040e13f5d9
Nico Britze <dietntexperten@gamil.com>
2020-08-19 12:28:29 +0200

    Fix Import

--- simplixcore-common/simplixcore-common-api/src/main/java/dev/simplix/core/common/permission/Permissions.java
+++ simplixcore-common/simplixcore-common-api/src/main/java/dev/simplix/core/common/permission/Permissions.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import dev.simplix.core.common.ApplicationInfo;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
