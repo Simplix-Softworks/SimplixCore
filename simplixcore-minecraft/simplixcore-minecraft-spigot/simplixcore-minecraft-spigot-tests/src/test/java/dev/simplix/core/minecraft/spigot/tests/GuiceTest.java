8aea34d3c0804fc5913f2602fde5f3b0e7f010ac
Exceptionflug <dietntexperten@gmail.com>
2020-08-30 16:58:14 +0200

    Add unit test for guice

--- simplixcore-minecraft/simplixcore-minecraft-spigot/simplixcore-minecraft-spigot-tests/src/test/java/dev/simplix/core/minecraft/spigot/tests/GuiceTest.java
+++ simplixcore-minecraft/simplixcore-minecraft-spigot/simplixcore-minecraft-spigot-tests/src/test/java/dev/simplix/core/minecraft/spigot/tests/GuiceTest.java
@@ -1,16 +1,90 @@
 package dev.simplix.core.minecraft.spigot.tests;
 
-public final class GuiceTest {
-//
-//  @Test
-//  public void testInstall() {
-// TODO
-//
-//    DependencyDownloader dependencyDownloader = guiceInstaller
-//        .getInjector()
-//        .getInstance(DependencyDownloader.class);
-//    Assertions.assertTrue(
-//        dependencyDownloader instanceof HttpDependencyDownloader,
-//        "Implementation was unexpected or null");
-//  }
+import com.google.inject.*;
+import dev.simplix.core.common.CommonSimplixModule;
+import dev.simplix.core.common.aop.AbstractSimplixModule;
+import dev.simplix.core.common.aop.Private;
+import dev.simplix.core.common.aop.ScanComponents;
+import dev.simplix.core.common.aop.SimplixApplication;
+import dev.simplix.core.common.inject.SimplixInstaller;
+import dev.simplix.core.common.providers.PluginManager;
+import dev.simplix.core.minecraft.spigot.SpigotSimplixModule;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GuiceTest {
+
+  @Test
+  public void testInstall() {
+    SimplixInstaller.instance().register(DependTestApplication.class);
+    SimplixInstaller.instance().register(TestApplication.class, new PrivacyTestModule(),
+        new CommonSimplixModule(), new SpigotSimplixModule());
+    SimplixInstaller.instance().install();
+
+    Injector unitTestInjector = SimplixInstaller.instance().injector(TestApplication.class);
+    Assertions.assertNotNull(unitTestInjector);
+
+    Injector dependTestInjector = SimplixInstaller.instance().injector(DependTestApplication.class);
+    Assertions.assertNotNull(dependTestInjector);
+
+    PluginManager pluginManager = unitTestInjector.getInstance(PluginManager.class);
+    Assertions.assertNotNull(pluginManager);
+
+    PluginManager pluginManager2 = dependTestInjector.getInstance(PluginManager.class);
+    if(pluginManager != pluginManager2) {
+      throw new IllegalStateException("PluginManagers are not the same instance!");
+    }
+
+    expect(() -> dependTestInjector.getInstance(Key.get(Car.class, Private.class)), ConfigurationException.class);
+    Assertions.assertEquals("Audi A6", unitTestInjector.getInstance(Key.get(Car.class, Private.class)).getName());
+  }
+
+  private void expect(Runnable runnable, Class<? extends Exception> clazz) {
+    try {
+      runnable.run();
+      throw new IllegalStateException(clazz.getName()+" did not occur");
+    } catch (Exception e) {
+      if(!e.getClass().equals(clazz)) {
+        throw e;
+      }
+    }
+  }
+
+  @SimplixApplication(name = "UnitTest", version = "1.0", authors = "SimplixSoftworks")
+  @ScanComponents("dev.simplix.core")
+  final static class TestApplication {
+  }
+
+  @SimplixApplication(name = "DependTest", version = "1.0", authors = "SimplixSoftworks",
+      dependencies = "UnitTest")
+  final static class DependTestApplication {
+  }
+
+  static class PrivacyTestModule extends AbstractSimplixModule {
+
+    @Provides
+    @Private
+    public Car somePrivateString() {
+      System.out.println("Hello binding");
+      Car car = new Car();
+      car.name = "Audi A6";
+      car.horsePower = 190;
+      return car;
+    }
+
+  }
+
+  @NoArgsConstructor
+  @Getter
+  static final class Car {
+
+    private String name;
+    private int horsePower;
+
+  }
+
 }
