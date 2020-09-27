52c726edb53d87f5ace472b051cb444f6c7ae818
Exceptionflug <dietntexperten@gmail.com>
2020-09-27 12:48:55 +0200

    Don't use preconditions since some guava version mismatch occurs on spigot

--- simplixcore-minecraft/simplixcore-minecraft-spigot/simplixcore-minecraft-spigot-plugin/src/main/java/dev/simplix/core/minecraft/spigot/plugin/util/PluginDescriptionUtil.java
+++ simplixcore-minecraft/simplixcore-minecraft-spigot/simplixcore-minecraft-spigot-plugin/src/main/java/dev/simplix/core/minecraft/spigot/plugin/util/PluginDescriptionUtil.java
@@ -1,6 +1,5 @@
 package dev.simplix.core.minecraft.spigot.plugin.util;
 
-import com.google.common.base.Preconditions;
 import java.io.File;
 import java.io.InputStream;
 import java.util.jar.JarEntry;
@@ -17,12 +16,9 @@ public final class PluginDescriptionUtil {
   public static PluginDescriptionFile loadPluginYml(File target) {
     try (JarFile jar = new JarFile(target)) {
       JarEntry pdf = jar.getJarEntry("plugin.yml");
-      Preconditions.checkNotNull(pdf, "Plugin must have a plugin.yml");
 
       try (InputStream in = jar.getInputStream(pdf)) {
         PluginDescriptionFile desc = new PluginDescriptionFile(in);
-        Preconditions.checkNotNull(desc.getName(), "Plugin from %s has no name", target);
-        Preconditions.checkNotNull(desc.getMain(), "Plugin from %s has no main", target);
         return desc;
       }
     } catch (Exception exception) {
