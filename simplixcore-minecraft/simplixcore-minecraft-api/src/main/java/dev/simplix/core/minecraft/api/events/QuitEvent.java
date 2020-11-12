37379eae6267c51959b57cb7ba6e7256bbcc64c2
Alexey Zakharov <zahar54rus@ya.ru>
2020-11-12 14:53:43 +0700

    fix import for successful build

--- simplixcore-minecraft/simplixcore-minecraft-api/src/main/java/dev/simplix/core/minecraft/api/events/QuitEvent.java
+++ simplixcore-minecraft/simplixcore-minecraft-api/src/main/java/dev/simplix/core/minecraft/api/events/QuitEvent.java
@@ -2,17 +2,20 @@ package dev.simplix.core.minecraft.api.events;
 
 import dev.simplix.core.common.event.AbstractEvent;
 import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Getter
 @Accessors(fluent = true)
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class QuitEvent extends AbstractEvent {
 
-  public static QuitEvent create(@NonNull final UUID target) {
-    return new QuitEvent(target);
-  }
+  private final UUID targetUUID;
 
-  private final UUID target;
+  public static QuitEvent create(@NonNull final UUID targetUUID) {
+    return new QuitEvent(targetUUID);
+  }
 }
