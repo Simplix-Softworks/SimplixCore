ca5a4e416e6ef9255fd26c7e781fc7320a48287f
Leonhard <javafactorydev@gmail.com>
2020-11-12 08:53:37 +0100

    Add imports to QuitEvent

--- simplixcore-minecraft/simplixcore-minecraft-api/src/main/java/dev/simplix/core/minecraft/api/events/QuitEvent.java
+++ simplixcore-minecraft/simplixcore-minecraft-api/src/main/java/dev/simplix/core/minecraft/api/events/QuitEvent.java
@@ -2,8 +2,11 @@ package dev.simplix.core.minecraft.api.events;
 
 import dev.simplix.core.common.event.AbstractEvent;
 import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Getter
 @Accessors(fluent = true)
